473886c97d76b4663076cf0309d2a071398cdac4
WiserOrb <diego@localhost.localdomain>
2024-06-12 15:03:28 +0200

    posit init

--- build.zig
+++ build.zig
@@ -2,10 +2,7 @@ const std = @import("std");
 
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const comath_mod = b.addModule("zigposit", .{
+    const posit_mod = b.addModule("zigposit", .{
         .root_source_file = b.path("src/posit.zig"),
     });
 }
