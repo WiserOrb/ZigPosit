89a9c268b8f26f645395cabe32dc6af39d21f084
WiserOrb <50355292+WiserOrb@users.noreply.github.com>
2024-06-12 15:05:46 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 
 pub fn build(b: *std.Build) void {
-    const posit_mod = b.addModule("zigposit", .{
+    _ = b.addModule("zigposit", .{
         .root_source_file = b.path("src/posit.zig"),
     });
 }
